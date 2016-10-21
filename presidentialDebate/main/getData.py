ce6254b4d4e03fe52cddb45f3dae9c5b233f2318
Rachel Lee <slee17@cmc.edu>
2016-10-20 17:45:24 -0700

    Updated getData.py to incorporate all three rounds

--- presidentialDebate/main/getData.py
+++ presidentialDebate/main/getData.py
@@ -1,24 +1,28 @@
 import re
 
-PREFIX = '../third/'
+# TODO: pass as command line argument
+FILES = ['./data/full0.txt', './data/full1.txt', './data/full2.txt']
+PREFIXES = ['./data/first/', './data/second/', './data/third/']
+SPLITS = ['(Holt:|Clinton:|Trump:)', '(Raddatz:|Cooper:|Clinton:|Trump:)', '(Wallace:|Clinton:|Trump:)']
+DELIMITERS = [{'Holt:', 'Clinton:', 'Trump:'}, {'Raddatz:', 'Cooper:', 'Clinton:', 'Trump:'}, {'Wallace:', 'Clinton:', 'Trump:'}]
 
-def extract(filename):
+def extract(filename, n):
 	with open(filename, 'r') as file:
 		content = file.read().replace('\n', ' ')
-	content = re.split('(Wallace:|Clinton:|Trump:)', content)
-	delimiters = {'Wallace:', 'Clinton:', 'Trump:'}
+	content = re.split(SPLITS[n], content)
+	delimiter = DELIMITERS[n]
 	i = 0
 	count = 0
-	print ("Extracting quotes...")
+	print ("Extracting quotes from %s" % filename)
 	while i < len(content):
 		j = i+1
 		current = ''
-		if content[i] in delimiters:
-			while j < len(content) and content[j] not in delimiters:
+		if content[i] in delimiter:
+			while j < len(content) and content[j] not in delimiter:
 				current += content[j]
 				j += 1
 			# write to a new file
-			filename = PREFIX + content[i][:-1].lower() + '_' + str(count) + '.txt'
+			filename = PREFIXES[n] + content[i][:-1].lower() + '_' + str(count) + '.txt'
 			new_file = open(filename, "w")
 			new_file.write(current)
 			new_file.close()
@@ -27,4 +31,7 @@ def extract(filename):
 	return
 
 if __name__ == "__main__":
-	temp = extract('full2.txt')
+	count = 0
+	for file in FILES:
+		extract(file, count)
+		count += 1
